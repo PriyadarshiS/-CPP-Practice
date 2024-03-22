a0b3316e652fbb0b8c05a778833cf424ecd3d051
Shubham Priyadarshi <119493227+PriyadarshiS@users.noreply.github.com>
2024-03-22 10:34:11 +0530

    Delete basicsOfc++ directory

--- basicsOfc++/input.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-int main(){
-    int apples;
-    cout<<"Enter no. of apples: ";
-    cin>>apples;
-    cout<<"Number of apples: "<<apples<<endl;
-    return 0;
-}
